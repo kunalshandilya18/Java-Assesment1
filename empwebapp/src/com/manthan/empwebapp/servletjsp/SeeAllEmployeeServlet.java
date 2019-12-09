6e8e03c14dc769bdd3c50af53a92bc105492b782
kunalshandilya18 <kunalshandilya18@gmail.com>
2019-12-09 17:08:49 +0530

    java-classroom

--- empwebapp/src/com/manthan/empwebapp/servletjsp/SeeAllEmployeeServlet.java
+++
@@ -1,8 +0,0 @@
-package com.manthan.empwebapp.servletjsp;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-@WebServlet("/seeAllEmployeeServletJsp")
-public class SeeAllEmployeeServlet extends HttpServlet {
-
-}
